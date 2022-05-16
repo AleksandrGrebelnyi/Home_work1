552e5af857d8399fbfd693747416f6b69d9186c8
AleksandrGrebelnyi <105456185+AleksandrGrebelnyi@users.noreply.github.com>
2022-05-16 07:57:46 +0300

    Update Operation_witn_two_numbers.py

--- Operation_witn_two_numbers.py
+++ Operation_witn_two_numbers.py
@@ -3,7 +3,7 @@
 
 num1 = int(input('Print any number: '))
 num2 = int(input('Print any number one more time: '))
-print('Sum of two numbers =', num1 + num2)
+print('Sum of two numbers =', num1 + num2)  # print(f'Sum of two numbers = {num1 + num2}')
 print('Multiply of two numbers =', num1 * num2)
 print('Difference of two numbers =', num1 - num2)
 print('Quotient of two numbers =', num1 / num2)
