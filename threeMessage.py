f51839bcfd75d8043d812fd087fd53c066b6a60d
AleksandrGrebelnyi <105456185+AleksandrGrebelnyi@users.noreply.github.com>
2022-05-16 07:59:50 +0300

    Update threeMessage.py

--- threeMessage.py
+++ threeMessage.py
@@ -1,2 +1,2 @@
 # Exercise 2. Rewrite the first script to display three any messages
-print('Hello my dear world.\nHow do you do? \nWhat is news?')
+print('Hello my dear world.\nHow do you do? \nWhat is news?')  # print('Hello my dear world.', 'How do you do?', 'What is news?', sep='\n')
