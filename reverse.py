d22cc5599e29c47a484de4bf47b2cf2d361341c5
AleksandrGrebelnyi <105456185+AleksandrGrebelnyi@users.noreply.github.com>
2022-05-24 21:43:44 +0300

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,7 @@
+# *Print the digits in reverse order
+
+num = 123
+nfirst = 123 // 100
+nlast = 123 % 10
+naverage = 123 % 100 // 10
+print('Reverse number ', num, '=', nlast, naverage, nfirst, sep='')  # print(int(str(nlast) + str(naverage) + str(nfirst)))
