75916a263bc889a17f8a81e94ac50ab2965e9a43
AleksandrGrebelnyi <105456185+AleksandrGrebelnyi@users.noreply.github.com>
2022-05-24 21:44:34 +0300

    Create sum_of_digits.py

---
+++ sum_of_digits.py
@@ -0,0 +1,10 @@
+# Write a Python-script that calculates the sum of the digits of a three-digit
+# number
+# я так понял, что задани одинаковые?
+# *Write a Python-script that calculates the sum of the digits of a number
+
+num = 123
+nfirst = 123 // 100
+nlast = 123 % 10
+naverage = 123 % 100 // 10
+print('Sum of three-digit-number =', nfirst + nlast + naverage)
