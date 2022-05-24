29e50dd21f89494ccd4270bb6f30520c4961907a
AleksandrGrebelnyi <105456185+AleksandrGrebelnyi@users.noreply.github.com>
2022-05-24 21:49:21 +0300

    Create largest_num.py

---
+++ largest_num.py
@@ -0,0 +1,5 @@
+# Write a Python program to find largest number among three numbers
+# entered by user
+
+n1, n2, n3 = int(input('Enter your number:')), int(input('Enter your number:')), int(input('Enter your number:'))
+print(max(n1, n2, n3))
